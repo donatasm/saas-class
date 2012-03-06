5b103ebad864edbb0f94a89aca71e6a629be1877
Donatas <donatas.mazionis@gmail.com>
2012-03-06 22:46:55 +0200

    Homework 2 part 1A

---
+++ hw2/part1/test_part1.rb
@@ -0,0 +1,24 @@
+
+require 'test/unit'
+require 'part1'
+
+
+class TestPart1A < Test::Unit::TestCase
+
+  def test_5_dollars_to_euro
+    assert_equal 5 * 1 / 1.292, 5.dollars.in(:euros)
+  end
+
+  def test_7_yen_to_dollar
+    assert_equal 7 * 0.013 / 1, 7.yens.in(:dollars)
+  end
+
+  def test_3_yen_to_euros
+    assert_equal 3 * 0.013 / 1.292, 3.yens.in(:euros)
+  end
+
+  def test_1_to_dollars
+    assert_equal 1, 1.dollar.in(:dollars)
+  end
+  
+end
